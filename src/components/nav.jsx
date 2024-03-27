6228732ab9876ec917458ee14a961bca5f840747
Ronnie Rios <ronniemarkrios@gmail.com>
2024-03-27 15:43:49 -0500

    clerk is cool

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 export default function Nav() {
+	const { user, isLoaded } = useUser();
+
 	return (
 		<header>
-			<nav className="max-w-2xl p-4 flex flex-row justify-between">
-				<Link to="/">
-					<a>Home</a>
-				</Link>
-				<Link to="/secret">
-					<a>Secret Link</a>
-				</Link>
+			<nav className="max-w-2xl border-y-2 mx-auto p-4 flex flex-row justify-between">
+				<Link href="/">Home</Link>
+				{isLoaded && user && 
+          (
+            <>
+            <Link href="/secret">Secret Link</Link>
+            <UserButton afterSignOutUrl="/" />
+            </>
+          )
+        }
 			</nav>
 		</header>
 	);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import Nav from "@/components/nav";
 import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
-			<Nav />
-			<Component {...pageProps} />;
+			<ClerkProvider {...pageProps}>
+				<Nav />
+				<Component {...pageProps} />
+			</ClerkProvider>
 		</>
 	);
 }
