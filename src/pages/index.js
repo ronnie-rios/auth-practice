da2d19ca7a8701ed1b6f3063ab38235e743159a6
Ronnie Rios <ronniemarkrios@gmail.com>
2024-03-29 14:19:08 -0500

    getserver session

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-    <h1>Home</h1> 
-    </main>
-  );
+export async function getServerSideProps(context) {
+  const session = await getServerSession(context.req, context.res)
+  return {
+    props: session
+  }
+}
+
+export default  function Home({ user }) {
+
+  console.log(user);
+	return (
+		<main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+			<h1>hello {user.name}</h1>
+		</main>
+	);
 }
